3a711eaa9c46dce98f5deab22195daab82700b84
Victor Dai <victor.dai.1@stonybrook.edu>
2021-02-20 22:48:13 -0500

    Created tab bar navigation and custom MovieGridCell UICollectionViewCell

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Victor Dai on 2/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
